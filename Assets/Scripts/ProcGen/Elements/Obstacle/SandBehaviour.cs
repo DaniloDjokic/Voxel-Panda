b60f8880c4afc867e25fea034c8d3dfbd7a14453
Danilo Djokic <danilo.djokic97@gmail.com>
2019-03-13 22:01:38 +0100

    All obstacle behaviors except cannon done

---
+++ Assets/Scripts/ProcGen/Elements/Obstacle/SandBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandBehaviour : LocalUpdater<Rigidbody>
+{
+    public float slowdownMultiplier;
+
+    private void FixedUpdate()
+    {
+        AddForceToRBs();
+    }
+
+    private void AddForceToRBs()
+    {
+        foreach (Rigidbody rb in currentlyContained)
+        {
+            rb.AddForce(rb.velocity * -1 * slowdownMultiplier);
+        }
+    }
+
+    protected override void PlayerEnters(GameObject player)
+    {
+        base.PlayerEnters(player);
+    }
+
+    protected override void PlayerExits(GameObject player)
+    {
+        base.PlayerExits(player);
+    }
+}
