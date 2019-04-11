5a544da976e257226f8f332328e258a75f127c1f
Sladjan MIlenovic <sladjko@gmail.com>
2019-04-11 14:15:25 +0200

    Added sounds

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,17 +14,38 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_COIN = 3114179205U;
+        static const AkUniqueID PLAY_CRUSHER = 2458249826U;
         static const AkUniqueID PLAY_GAMEPLAYMUSIC = 2481433679U;
         static const AkUniqueID PLAY_MENUMUSIC = 2570041344U;
+        static const AkUniqueID PLAY_ROBOT_ARM = 3759318933U;
         static const AkUniqueID PLAY_SKATE = 1618563948U;
         static const AkUniqueID PLAY_SWIPE_PULL = 3678449574U;
         static const AkUniqueID PLAY_SWIPE_RELEASE = 2803607158U;
+        static const AkUniqueID PLAY_UI_CLICK = 1749424733U;
         static const AkUniqueID PLAY_WALLKICK = 2567365808U;
+        static const AkUniqueID SAND_ENTER = 3025617480U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYERLIFE
+        {
+            static const AkUniqueID GROUP = 444815956U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+            } // namespace STATE
+        } // namespace PLAYERLIFE
+
+    } // namespace STATES
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID SIDE_CHAIN = 479396230U;
         static const AkUniqueID SKATE_SPEED = 3490328425U;
+        static const AkUniqueID SOUND_PITCH = 2100146717U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -35,7 +56,10 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID CHARACTER = 436743010U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
